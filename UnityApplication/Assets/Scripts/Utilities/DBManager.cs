5aae59badc24dcf9922d8f2cff813f6e248adc7d
Thomas WEBER <alpha.tbw@gmail.com>
2025-01-17 15:00:54 +0100

    init db

--- UnityApplication/Assets/Scripts/Utilities/DBManager.cs
+++ UnityApplication/Assets/Scripts/Utilities/DBManager.cs
@@ -1,13 +1,30 @@
 using UnityEngine;
+using MySql.Data.MySqlClient;
 
 public class DBManger : MonoBehaviour
 {
-    private string DB_Path { get; set; }
+    private string Server { get; } = "localhost";
+    private string Database { get; } = "UserStatsProject";
+    private string User { get; } = "UnityUser";
+    private string Password { get; } = "Unity123!";
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        DB_Path = $"";
+        string connectionString = $"Server={Server};Database={Database};User ID={User};Password={Password};Pooling=true;";
+
+        try
+        {
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+                Console.WriteLine("Connexion réussie à la base de données !");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Erreur : {ex.Message}");
+        }
     }
 
     // Update is called once per frame

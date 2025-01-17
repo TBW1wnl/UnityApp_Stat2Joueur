3f5f22de7346d7ed56121da563e8274615444b7c
Thomas WEBER <alpha.tbw@gmail.com>
2025-01-17 14:36:32 +0100

    création du projet unity

---
+++ UnityApplication/Assets/Scripts/Utilities/DBManager.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class DBManger : MonoBehaviour
+{
+    private string DB_Path { get; set; }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        DB_Path = $"";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
